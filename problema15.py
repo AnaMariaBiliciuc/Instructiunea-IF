45f9ea3cba0c8c044d09c58093f694a94da6ade0
AnaMariaBiliciuc <70842937+AnaMariaBiliciuc@users.noreply.github.com>
2020-10-18 20:12:11 +0300

    Add files via upload

---
+++ problema15.py
@@ -0,0 +1,13 @@
+x=int(input("Introduceti locul lui Radu:"))
+
+if x<=125:
+    if x>=100:
+        print("E")
+    elif x>=76:
+        print("D")
+    elif x>=51:
+        print("C")
+    elif x>=26:
+        print("B")
+else:
+        print("A")
