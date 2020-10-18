00bb5ff2671c597456f9a3b21a1ad373852d3620
AnaMariaBiliciuc <70842937+AnaMariaBiliciuc@users.noreply.github.com>
2020-10-18 19:33:08 +0300

    Add files via upload

---
+++ problema12.py
@@ -0,0 +1,11 @@
+x=int(input("Introduceti numarul petalei:"))
+if x%5==0:
+    print("... de loc.")
+elif x%4==0:
+    print("... la nebunie.")
+elif x%3==0:
+    print("... cu pasiune.")
+elif x%2==0:
+    print("... mult.")
+else:
+    print("... un pic.")
