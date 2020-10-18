101ddeeb4f20db24294080138ced9956d92c19ff
AnaMariaBiliciuc <70842937+AnaMariaBiliciuc@users.noreply.github.com>
2020-10-18 18:38:55 +0300

    Add files via upload

---
+++ problema4.py
@@ -0,0 +1,9 @@
+v1=int(input("Introduceti varsta 1: "))
+v2=int(input("Introduceti varsta nr 2: "))
+v3=int(input("Introduceti varsta nr 3: "))
+if (v1>18) and (v2<60) :
+    print(v1)
+if (v2>18) and (v2<60) :
+    print(v2)
+if (v3>18) and (v3<60) :
+    print(v3)
